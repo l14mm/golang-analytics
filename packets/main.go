f9562cc59c90320c67cf16edbdd61a1d5d06b324
liammoroney <liam.moroney14@gmail.com>
2019-01-26 14:53:16 +0000

    Reworked code for printing packets. Able to analyse specific layers now. Added methods to check for target ip's/mac addresses.

--- packets/main.go
+++ packets/main.go
@@ -1,63 +1,138 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 	"time"
 
-	"github.com/google/gopacket/dumpcommand"
-	"github.com/google/gopacket/examples/util"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
-var iface = flag.String("i", "eth0", "Interface to read packets from")
-var fname = flag.String("r", "", "Filename to read from, overrides -i")
-var snaplen = flag.Int("s", 65536, "Snap length (number of bytes max to read per packet")
-var tstype = flag.String("timestamp_type", "", "Type of timestamps to use")
-var promisc = flag.Bool("promisc", true, "Set promiscuous mode")
+var (
+	device            = "en0"
+	snapshotLen int32 = 1024
+	promiscuous       = false
+	err         error
+	timeout     = 30 * time.Second
+	handle      *pcap.Handle
+)
 
 func main() {
-	defer util.Run()()
-	var handle *pcap.Handle
-	var err error
-	if *fname != "" {
-		if handle, err = pcap.OpenOffline(*fname); err != nil {
-			log.Fatal("PCAP OpenOffline error:", err)
-		}
-	} else {
-		inactive, err := pcap.NewInactiveHandle(*iface)
-		if err != nil {
-			log.Fatalf("could not create: %v", err)
-		}
-		defer inactive.CleanUp()
-		if err = inactive.SetSnapLen(*snaplen); err != nil {
-			log.Fatalf("could not set snap length: %v", err)
-		} else if err = inactive.SetPromisc(*promisc); err != nil {
-			log.Fatalf("could not set promisc mode: %v", err)
-		} else if err = inactive.SetTimeout(time.Second); err != nil {
-			log.Fatalf("could not set timeout: %v", err)
-		}
-		if *tstype != "" {
-			if t, err := pcap.TimestampSourceFromString(*tstype); err != nil {
-				log.Fatalf("Supported timestamp types: %v", inactive.SupportedTimestamps())
-			} else if err := inactive.SetTimestampSource(t); err != nil {
-				log.Fatalf("Supported timestamp types: %v", inactive.SupportedTimestamps())
-			}
-		}
-		if handle, err = inactive.Activate(); err != nil {
-			log.Fatal("PCAP Activate error:", err)
-		}
-		defer handle.Close()
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
 	}
-	if len(flag.Args()) > 0 {
-		bpffilter := strings.Join(flag.Args(), " ")
-		fmt.Fprintf(os.Stderr, "Using BPF filter %q\n", bpffilter)
-		if err = handle.SetBPFFilter(bpffilter); err != nil {
-			log.Fatal("BPF filter error:", err)
+	defer handle.Close()
+
+	targetIP1 := ""
+	targetIP2 := ""
+
+	targetMAC1 := ""
+	targetMAC2 := ""
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		if isIPTarget(packet, targetIP1, targetIP2) {
+			printPacketInfo(packet)
+		}
+		if isMacTarget(packet, targetMAC1, targetMAC2) {
+			printPacketInfo(packet)
 		}
 	}
-	dumpcommand.Run(handle)
+}
+
+func isMacTarget(packet gopacket.Packet, targetMAC1 string, targetMAC2 string) bool {
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		if ethernetPacket.SrcMAC.String() == targetMAC1 && ethernetPacket.DstMAC.String() == targetMAC2 {
+			return true
+		}
+		if ethernetPacket.DstMAC.String() == targetMAC1 && ethernetPacket.SrcMAC.String() == targetMAC2 {
+			return true
+		}
+	}
+	return false
+}
+
+func isIPTarget(packet gopacket.Packet, targetIP1 string, targetIP2 string) bool {
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		ip, _ := ipLayer.(*layers.IPv4)
+		if ip.SrcIP.String() == targetIP1 && ip.DstIP.String() == targetIP2 {
+			return true
+		}
+		if ip.DstIP.String() == targetIP1 && ip.SrcIP.String() == targetIP2 {
+			return true
+		}
+	}
+	return false
+}
+
+func printPacketInfo(packet gopacket.Packet) {
+	fmt.Println("******packet******")
+
+	// Let's see if the packet is an ethernet packet
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		fmt.Println("Ethernet layer detected.")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		// Ethernet type is typically IPv4 but could be ARP or other
+		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is IP (even though the ether type told us)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+
+		// IP layer variables:
+		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is TCP
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		fmt.Println("TCP layer detected.")
+		tcp, _ := tcpLayer.(*layers.TCP)
+
+		// TCP layer variables:
+		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println()
+	}
+
+	// Iterate over all layers, printing out each layer type
+	fmt.Println("All packet layers:")
+	for _, layer := range packet.Layers() {
+		fmt.Println("- ", layer.LayerType())
+	}
+
+	// When iterating through packet.Layers() above,
+	// if it lists Payload layer then that is the same as
+	// this applicationLayer. applicationLayer contains the payload
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+		fmt.Println("Application layer/Payload found.")
+		fmt.Printf("%s\n", applicationLayer.Payload())
+
+		// Search for a string inside the payload
+		if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
+			fmt.Println("HTTP found!")
+		}
+	}
+
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
 }
